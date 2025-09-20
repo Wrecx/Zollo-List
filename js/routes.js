1a86ba3c727b31d2c7ab9236e2f35ea7a9f2a403
Wrecx <85760230+Wrecx@users.noreply.github.com>
2025-09-20 20:53:05 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -3,7 +3,7 @@ import Leaderboard from 'pages/Leaderboard.js';
 import Roulette from 'pages/Roulette.js';
 
 export default [
-    { path: '/Zollo-List/', component: List },
-    { path: '/Zollo-List/leaderboard', component: Leaderboard },
-    { path: '/Zollo-List/roulette', component: Roulette },
+    { path: '/', component: List },
+    { path: '/leaderboard', component: Leaderboard },
+    { path: '/roulette', component: Roulette },
 ];
